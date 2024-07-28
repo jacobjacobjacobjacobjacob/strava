35b3d43424d3a15409d7dce85a1e2a80e7833613
jacobjacobjacobjacobjacob <jacob.schjetne@gmail.com>
2024-07-28 10:12:24 +0200

    generate access token, api call

---
+++ api/api.py
@@ -0,0 +1,26 @@
+# strava/api.py
+import requests
+from assets.config import setup_logging
+from auth import get_strava_tokens
+from assets.utils import save_to_csv
+
+
+setup_logging()
+
+
+def get_strava_activities():
+    """Fetches all Strava activities"""
+    access_token = get_strava_tokens()
+    if access_token is None:
+        raise Exception("Failed to retrieve access token.")
+    activities_url = "https://www.strava.com/api/v3/athlete/activities"
+    headers = {"Authorization": "Bearer " + access_token}
+    params = {"per_page": 200, "page": 1}
+
+    try:
+        response = requests.get(activities_url, headers=headers, params=params)
+        response.raise_for_status()  # Raise exception for non-200 status codes
+        return response.json()
+    except requests.exceptions.RequestException as e:
+        logger.error("Error occurred while fetching activities: %s", e)
+        return None
---
+++ api/auth.py
@@ -0,0 +1,55 @@
+# api/auth.py
+import os
+import requests
+import logging
+from dotenv import load_dotenv
+from assets.config import setup_logging
+
+# Load logging configuration
+setup_logging()
+logger = logging.getLogger(__name__)
+
+# Load credentials from .env file
+load_dotenv()
+strava_client_id = os.getenv("STRAVA_CLIENT_ID")
+strava_client_secret = os.getenv("STRAVA_CLIENT_SECRET")
+strava_refresh_token = os.getenv("STRAVA_REFRESH_TOKEN")
+
+
+def get_strava_tokens() -> str:
+    """
+    Fetches a new Strava access token using the refresh token.
+
+    :return: Access token as a string if successful, None otherwise.
+    """
+    token_url = "https://www.strava.com/oauth/token"
+    refresh_payload = {
+        "client_id": strava_client_id,
+        "client_secret": strava_client_secret,
+        "refresh_token": strava_refresh_token,
+        "grant_type": "refresh_token",
+    }
+    try:
+        response = requests.post(token_url, data=refresh_payload)
+        response.raise_for_status()  # Raise exception for non-200 status codes
+        data = response.json()
+        access_token = data.get("access_token")
+        if access_token:
+            logger.info("Successfully retrieved new access token.")
+        else:
+            logger.warning("Access token not found in the response.")
+        return access_token
+    except requests.exceptions.RequestException as e:
+        logger.error("Error occurred while refreshing token: %s", e)
+        return None
+
+
+if __name__ == "__main__":
+    # Check for missing env varrs
+    if not all([strava_client_id, strava_client_secret, strava_refresh_token]):
+        logger.error("Strava credentials missing.")
+
+    token = get_strava_tokens()
+
+    if not token:
+        logger.error("Failed to retrieve access token.")
