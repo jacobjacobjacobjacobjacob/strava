64c4c10e9e44547b9a1a889fb1c45b9ae4e42996
jacobjacobjacobjacobjacob <jacob.schjetne@gmail.com>
2024-07-28 10:28:59 +0200

    path fix

--- assets/utils.py
+++ assets/utils.py
@@ -2,20 +2,30 @@
 import pandas as pd
 import numpy as np
 import os
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 def save_to_csv(df: pd.DataFrame, file_name: str) -> None:
     """
     Save a DataFrame to a CSV file.
-
-    :param df: DataFrame to save.
-    :param file_name: Base name of the file (without extension).
     """
+    try:
+        base_dir = os.path.abspath(
+            os.path.join(os.path.dirname(__file__), "..", "..", "strava", "data")
+        )
 
-    directory = os.path.join(os.path.dirname(__file__), "..", "..", "data")
+        if not os.path.exists(base_dir):
+            os.makedirs(base_dir)
+            logger.info(f"Created directory: {base_dir}")
 
-    if not os.path.exists(directory):
-        os.makedirs(directory)
+        file_path = os.path.join(base_dir, f"{file_name}.csv")
 
-    file_path = os.path.join(directory, f"{file_name}.csv")
-    df.to_csv(file_path, index=False)
+        # Save DataFrame to CSV
+        df.to_csv(file_path, index=False)
+        logger.info(f"DataFrame successfully saved to {file_path}")
+
+    except Exception as e:
+        logger.error(f"Failed to save DataFrame to CSV: {e}")
+        raise
