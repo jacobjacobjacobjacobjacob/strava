221a41a246c32965286ffc3efc14558f28fae59c
jacobjacobjacobjacobjacob <jacob.schjetne@gmail.com>
2024-07-28 10:03:36 +0200

    initial project structure

---
+++ assets/config.py
@@ -0,0 +1,18 @@
+# assets/config.py
+import pytz
+import logging
+
+# Strava gear_id's, representing different types of gear, in this case spinning bike/outdoor road bike.
+gear = {
+    "b14008209": "indoor",
+    "b12658885": "outdoor",
+}
+
+local_tz = pytz.timezone("Europe/Oslo")  # Timezone
+
+
+def setup_logging():
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    )
