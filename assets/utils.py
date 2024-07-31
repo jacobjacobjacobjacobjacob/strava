4d9b2c6735e9b0be6c098815e66ee384d2911000
jacobjacobjacobjacobjacob <jacob.schjetne@gmail.com>
2024-07-31 10:50:49 +0200

    added month mapping dict

--- assets/utils.py
+++ assets/utils.py
@@ -116,3 +116,18 @@ all_months = [
     "nov",
     "dec",
 ]
+
+month_mapping = {
+    1: "jan",
+    2: "feb",
+    3: "mar",
+    4: "apr",
+    5: "may",
+    6: "jun",
+    7: "jul",
+    8: "aug",
+    9: "sep",
+    10: "oct",
+    11: "nov",
+    12: "dec",
+}
