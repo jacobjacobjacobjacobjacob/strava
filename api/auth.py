2948a5fa735f4fb1c3e57bacf5ed12e36c257b8d
jacobjacobjacobjacobjacob <jacob.schjetne@gmail.com>
2024-07-31 19:10:52 +0200

    added function to write new tokens to .env file

--- api/auth.py
+++ api/auth.py
@@ -2,7 +2,7 @@
 import os
 import requests
 import logging
-from dotenv import load_dotenv
+from dotenv import load_dotenv, set_key
 from assets.config import setup_logging
 
 # Load logging configuration
@@ -14,11 +14,12 @@ load_dotenv()
 strava_client_id = os.getenv("STRAVA_CLIENT_ID")
 strava_client_secret = os.getenv("STRAVA_CLIENT_SECRET")
 strava_refresh_token = os.getenv("STRAVA_REFRESH_TOKEN")
+strava_access_token = os.getenv("token")
 
 
 def get_strava_tokens() -> str:
     """
-    Fetches a new Strava access token using the refresh token.
+    Fetches a new Strava access token using the refresh token and updates the .env file.
 
     :return: Access token as a string if successful, None otherwise.
     """
@@ -29,13 +30,21 @@ def get_strava_tokens() -> str:
         "refresh_token": strava_refresh_token,
         "grant_type": "refresh_token",
     }
+
     try:
         response = requests.post(token_url, data=refresh_payload)
         response.raise_for_status()  # Raise exception for non-200 status codes
         data = response.json()
         access_token = data.get("access_token")
+
         if access_token:
             logger.info("Successfully retrieved new access token.")
+
+            # Load existing .env file and update the access token
+            env_file = ".env"
+            load_dotenv(env_file)
+            set_key(env_file, "token", access_token)
+            logger.info("Access token written to .env file.")
         else:
             logger.warning("Access token not found in the response.")
         return access_token
