827eef233f0e43906f928875221c6fd00e204d31
jacobjacobjacobjacobjacob <jacob.schjetne@gmail.com>
2025-03-21 13:09:32 +0100

    dedicated config file for apis

---
+++ src/api/config.py
@@ -0,0 +1,14 @@
+# src/api/config.py
+""" API configurations """
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+def get_strava_config():
+    strava_config = {
+        "client_id": os.getenv("STRAVA_CLIENT_ID"),
+        "client_secret": os.getenv("STRAVA_CLIENT_SECRET"),
+        "refresh_token": os.getenv("STRAVA_REFRESH_TOKEN"),
+        "athlete_id": os.getenv("STRAVA_ATHLETE_ID"),
+    }
+    return strava_config
