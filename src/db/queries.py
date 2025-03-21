2c9bdfd9e87fb8f6648034b37c362265c659c873
jacobjacobjacobjacobjacob <jacob.schjetne@gmail.com>
2025-03-21 15:25:03 +0100

    query to drop some rows for debugging

--- src/db/queries.py
+++ src/db/queries.py
@@ -8,6 +8,7 @@ ALLOWED_TABLES = [
     "zones",
     "cache",
     "streams",
+    "health",
 ]
 INSERT_ID_TO_CACHE = """
         INSERT OR REPLACE INTO cache (id)
@@ -99,6 +100,40 @@ CREATE_ALL_TABLES = {
                     id INTEGER PRIMARY KEY
                 )
             """,
+    "health": """
+    CREATE TABLE IF NOT EXISTS health (
+        id,
+        date TEXT PRIMARY KEY,
+        apple_exercise_time REAL,
+        apple_move_time REAL,
+        caffeine REAL,
+        cardio_recovery REAL,
+        flights_climbed INTEGER,
+        headphone_audio_exposure REAL,
+        heart_rate_variability REAL,
+        mindful_minutes REAL,
+        physical_effort REAL,
+        respiratory_rate REAL,
+        resting_heart_rate REAL,
+        running_ground_contact_time REAL,
+        running_power REAL,
+        running_speed REAL,
+        running_stride_length REAL,
+        running_vertical_oscillation REAL,
+        sleep_analysis_asleep REAL,
+        sleep_analysis_in_bed REAL,
+        sleep_analysis_core REAL,
+        sleep_analysis_deep REAL,
+        sleep_analysis_rem REAL,
+        sleep_analysis_awake REAL,
+        step_count INTEGER,
+        time_in_daylight REAL,
+        vo2_max REAL,
+        walking_running_distance REAL,
+        month TEXT,
+        day_of_week TEXT
+    )
+"""
 }
 
 
@@ -106,8 +141,8 @@ INSERT_OR_IGNORE_QUERY = (
     "INSERT OR IGNORE INTO {table_name} ({columns}) VALUES ({placeholders})"
 )
 
-
 GET_CACHED_IDS = "SELECT id FROM cache;"
+GET_DATES_FROM_HEALTH = "SELECT date FROM health;"
 
 CLEAR_CACHE = "DELETE FROM cache;"
 
@@ -118,5 +153,12 @@ GET_ZONES_IDS = "SELECT id FROM zones;"
 GET_SPLITS_IDS = "SELECT id FROM splits;"
 GET_STREAMS_IDS = "SELECT id FROM streams;"
 GET_BEST_EFFORTS_IDS = "SELECT id FROM best_efforts;"
+GET_GEAR_IDS = "SELECT gear_id FROM gear;"
 
 READ_TABLE_TO_DF = "SELECT * FROM {table_name};"
+
+
+DELETE_LAST_ROW = """
+    DELETE FROM {table_name}
+    WHERE id = (SELECT id FROM {table_name} ORDER BY id DESC LIMIT 1);
+"""
