a34ae92c10ec770c949c4dc935ac94e90f45292f
jacobjacobjacobjacobjacob <jacob.schjetne@gmail.com>
2025-04-01 08:40:19 +0200

    centralized modules for loading data from the database

--- src/analysis/loading.py
+++ src/analysis/loading.py
@@ -1,5 +1,8 @@
 # src/analysis/loading.py
 import pandas as pd
+import warnings
+
+warnings.simplefilter(action="ignore", category=FutureWarning)
 from loguru import logger
 import calendar
 
@@ -9,12 +12,20 @@ from src.db.db_manager import DatabaseManager
 db_manager = DatabaseManager()
 activities_df = db_manager.get_table_as_dataframe("activities")
 
+
 def process_activity_dataframe(df: pd.DataFrame):
     # Convert 'month' column (e.g., '01' → 'Jan')
-    df['month'] = df['month'].apply(lambda x: calendar.month_abbr[int(x)])
+    df["month"] = df["month"].apply(lambda x: calendar.month_abbr[int(x)])
+
     # Convert "duration" to hours.
     df["duration"] = round((df["duration"] / 60), 0)
+
+    # Add year column
+    # df["date"] = pd.to_datetime(df["date"])
+    # df["year"] = pd.to_datetime(df["date"]).dt.year.astype('int32')
+    df["year"] = pd.to_datetime(df["date"]).dt.year.astype("int32")
     return df
+
 
 def load_dataframes():
     db_manager = DatabaseManager()
